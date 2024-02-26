99132c7fb05d8a139c49892df891127ba3a01432
sanaullah <sanaullah2209f@aptechgdn.net>
2024-02-27 00:57:27 +0500

    webhooks

--- middleware.ts
+++ middleware.ts
@@ -2,8 +2,7 @@ import { authMiddleware } from "@clerk/nextjs";
  
 export default authMiddleware({
 
-  publicRoutes: ['/api/webhooks/clerk'],                              ]
-
+  publicRoutes: ['/api/webhooks/clerk']
 });
  
 export const config = {
